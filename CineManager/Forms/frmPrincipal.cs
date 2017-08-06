3077b65c1d9f1431a1ad39395880df3f826e8387
Ronye <ronye.rocha@gmail.com>
2017-08-06 15:28:07 -0300

    sobre...

--- CineManager/Forms/frmPrincipal.cs
+++ CineManager/Forms/frmPrincipal.cs
@@ -346,7 +346,7 @@ namespace CineManager
 
         private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Desenvolvido por\n\n Ronye do Lago Cornetti Rocha - 2017", "Sobre");
+            MessageBox.Show("Desenvolvido por\n\n Ronye do Lago Cornetti Rocha\n\n 2017", "Sobre");
         }
     }
 }
